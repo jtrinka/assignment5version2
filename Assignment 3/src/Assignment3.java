96d0bee049087fd30a886871b0be7dbb9aef86ee
jtrinka <jtrinka@CPU2090.carroll.edu>
2015-09-15 20:11:55 -0600

    Fourth coding attempt

--- Assignment 3/src/Assignment3.java
+++ Assignment 3/src/Assignment3.java
@@ -1,11 +1,36 @@
 import java.security.SecureRandom;
 import java.util.Random;
 import java.util.Scanner;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
 public class Assignment3 {
 //
 	
 	//
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		
+		for(int i = 0; i < 10; i++)
+			{
+			g.drawRect(10 + i *10, 10+i *10, 50+i * 10, 50+i * 10);
+			g.drawOval(240+i * 10, 10 + i * 10, 50 + i * 10, 50 + i * 10);
+			
+			}
+		
+	}
 	public static void main(String[] args) {
+		
+		Shapes panel = new Shapes();
+		JFrame application = new JFrame();
+		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(500,  290);
+		application.setVisible(true);
+		
+		
 		Scanner input = new Scanner(System.in);
 		int count=1;
 		while(count < 3 )
@@ -41,7 +66,7 @@ public class Assignment3 {
 	catch (Exception e){};
 	System.out.println("Roll a 1, no points gained or lost. 2 gain 2 points. 3 lose 1 point. 4 roll gain 4 points. 5 your opponent gets 5 points. 6 get 6 points. First one to 10 points or over wins");
 	
-	for (i = 1; i<1000; i++) //start at i is equal to 1 and run until i is equal to 10. Don't run a for loop with a double or float runs 9 times.
+	for (i = 1; i<2; i++) //start at i is equal to 1 and run until i is equal to 10. Don't run a for loop with a double or float runs 9 times.
 	{
 		pick = mySecureRandomgenerator.nextInt(6)+1; //set pick to 0 but we make it equal to any random number
 		try{Thread.sleep(1500);}
@@ -74,9 +99,31 @@ public class Assignment3 {
 							System.out.println("Gain 6 Points");
 	}
 	myScanner.close();
+	//weblogs.java.net
+	Random rand = new Random();
+    int z = rand.nextInt(50);
+    int y = rand.nextInt(50);
+    System.out.println("(z > 5) && (y > 5) is " + ((z > 5) && (y > 5)) );
+    System.out.println("(z < 5) || (y < 5) is " +  ((z < 5) || (y < 5)) );
+    
+    // Best for 1 variable
+    //int a=0;
+	//switch( a ) {
+    //case 1: System.out.print("\nNow I'll print one");
+    //System.out.println("-and that is a good choice");
+    //break causes it to jump out of block
+    //case 2-5: System.out.println("\nTwo");
+    //break;
+    //default: System.out.println("\neither one or two");
+    //break;
+	//}
+    
+ 
+    
+	
 	return;
 	
-
+	
 }
 	
 
